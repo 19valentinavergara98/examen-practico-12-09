620d7acf96e109da32be55a1315b202cfe76ee95
Valentina Vergara <valentina.98.3440@gmail.com.com>
2024-09-12 11:15:38 +0200

    included 3 files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "haversine.h"
+
+int main() {    
+    // Sagrada Familia
+    double lat1 = 41.4036;
+    double lon1 = 2.1744;
+
+    // Parque Güell
+    double lat2 = 41.4145;
+    double lon2 = 2.1527;
+
+    double distancia = calcularDistancia(lat1, lon1, lat2, lon2);
+
+    std::cout << "La distancia entre Sagrada Familia y Parque Güell es: "
+              << distancia << " km." << std::endl;
+
+    return 0;
+}
